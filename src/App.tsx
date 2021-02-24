bf26c44a5bed84637c67b33976d761ba3bb0df2e
Marvin Schopf <marvin@schopf.biz>
2021-02-24 16:10:19 +0100

    add basic firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import Router from 'preact-router';
 import { h, render } from 'preact';
+import firebase from "firebase/app";
+import "firebase/auth";
 /** @jsx h */
 
 import Home from "./routes/Home";
+import { FirebaseAuthProvider } from '@react-firebase/auth';
+
+import firebaseConfig from "./config/firebase";
 
 export default function Controller() {
 
     return (
-        <Router>
-            <Home path="/" />
-        </Router>
+        <FirebaseAuthProvider
+            firebase={firebase}
+            {...firebaseConfig}
+        >
+            <Router>
+                <Home path="/" />
+            </Router>
+        </FirebaseAuthProvider>
     );
 }
