76a111f415f2b9ad0001d96c2d2fcc10665b17bb
Marvin Schopf <marvin@schopf.biz>
2021-02-24 11:19:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from 'preact';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>
+                It works!
+            </h1>
+        );
+    }
+}
+
+export default App;
