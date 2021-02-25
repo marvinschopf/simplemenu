313699fddd40507c8958a4ea220afe7776cfc668
Marvin Schopf <marvin@schopf.biz>
2021-02-25 10:00:48 +0100

    use blueprint as ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,8 @@ import { FirebaseAuthProvider } from "@react-firebase/auth"
 
 import firebaseConfig from "./config/firebase"
 
-import Container from "react-bootstrap/Container"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import Button from "react-bootstrap/Button"
 import Login from "./routes/Login"
+import { Alignment, Navbar, Button } from "@blueprintjs/core"
 
 let plausible: any | false = false
 
@@ -30,52 +27,41 @@ if (typeof document !== "undefined" && typeof window !== "undefined") {
 export default function Controller() {
 	return (
 		<FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-			{/*@ts-ignore*/}
-			<Container>
-				<Router>
-					{/*@ts-ignore*/}
-					<Navbar bg="light">
-						{/*@ts-ignore*/}
-						<Navbar.Brand
-							onClick={() => {
-								route("/")
-							}}
-						>
-							SimpleMenu
-						</Navbar.Brand>
-						{/*@ts-ignore*/}
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						{/*@ts-ignore*/}
-						<Navbar.Collapse id="basic-navbar-nav">
-							{/*@ts-ignore*/}
-							<Nav className="mr-auto"></Nav>
-							{/*@ts-ignore*/}
-							<Nav>
-								{/*@ts-ignore*/}
-								<Button
-									onClick={() => {
-										route("/login")
-									}}
-								>
-									Login
-								</Button>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
-					<Login path="/login" />
-					<Home path="/" />
-				</Router>
-				<p>
-					<small>
-						Version:{" "}
-						<a
-							href={`https://github.com/marvinschopf/simplemenu/commit/${process.env.APP_CURRENT_COMMIT_HASH}`}
-						>
-							{process.env.APP_CURRENT_COMMIT_HASH_SHORT}
-						</a>
-					</small>
-				</p>
-			</Container>
+			<Navbar>
+				<Navbar.Group align={Alignment.LEFT}>
+					<Navbar.Heading
+						onClick={() => {
+							route("/")
+						}}
+					>
+						SimpleMenu
+					</Navbar.Heading>
+					<Navbar.Divider />
+				</Navbar.Group>
+				<Navbar.Group align={Alignment.RIGHT}>
+					<Button
+						className="bp3-minimal"
+						text="Login"
+						onClick={() => {
+							route("/login")
+						}}
+					/>
+				</Navbar.Group>
+			</Navbar>
+			<Router>
+				<Login path="/login" />
+				<Home path="/" />
+			</Router>
+			<p>
+				<small>
+					Version:{" "}
+					<a
+						href={`https://github.com/marvinschopf/simplemenu/commit/${process.env.APP_CURRENT_COMMIT_HASH}`}
+					>
+						{process.env.APP_CURRENT_COMMIT_HASH_SHORT}
+					</a>
+				</small>
+			</p>
 		</FirebaseAuthProvider>
 	)
 }
