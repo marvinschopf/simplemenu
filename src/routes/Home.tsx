c5ea7a93d28501e35cdd6aba924368da476e85d7
Marvin Schopf <marvin@schopf.biz>
2021-02-25 11:40:35 +0100

    fix name of home component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,9 @@
 import { Component, h } from "preact"
 
-class App extends Component {
+class Home extends Component {
 	render() {
 		return <h1>It works!</h1>
 	}
 }
 
-export default App
+export default Home
